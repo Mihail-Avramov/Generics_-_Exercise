84f8d1ac4b4d19802f488621b9430f05feb3787d
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-02-11 18:17:48 +0200

    3

---
+++ GenericSwapMethodString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Box<string> box = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                box.Add(value);
+            }
+
+            int[] commandArguments = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            box.Swap(commandArguments[0], commandArguments[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
