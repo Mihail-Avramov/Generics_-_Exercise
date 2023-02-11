cfbb593622370539379dc81e4c6f79867d6c3930
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-02-11 19:10:31 +0200

    6

---
+++ GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericCountMethodDoubles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double value = double.Parse(Console.ReadLine());
+                box.Add(value);
+            }
+
+            double itemToCompare = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(box.Count(itemToCompare));
+        }
+    }
+}
