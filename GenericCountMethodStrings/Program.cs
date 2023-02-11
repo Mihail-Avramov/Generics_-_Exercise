cb1871ff3bcb7dafb766d1e097b60bf3a0504c2a
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-02-11 18:42:28 +0200

    5

--- GenericCountMethodStrings/Program.cs
+++ GenericCountMethodStrings/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace GenericCountMethodStrings
+﻿using System;
+
+namespace GenericCountMethodStrings
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Box<string> box = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                box.Add(value);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.Count(itemToCompare));
         }
     }
 }
