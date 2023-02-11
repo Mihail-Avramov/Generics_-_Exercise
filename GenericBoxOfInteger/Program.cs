39aba3f0b18745a0a14297ed2d4467a283d5fbbc
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-02-11 17:50:03 +0200

    2

---
+++ GenericBoxOfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Box<int> box = new Box<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                box.Add(value);
+            }
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
